546d4a1d3616afbf3a0ad62a4880402a8befe91a
JiAyiX <jiayiwei9@gmail.com>
2024-10-15 07:12:22 +0200

    Mensaje describiendo los cambios

--- inst.bat
+++ inst.bat
@@ -9,10 +9,12 @@ if %errorLevel% neq 0 (
 )
 color 06
 cls
+for /F "tokens=2 delims=\" %%i in ('whoami') do set USER=%%i
+cd C:\Users\%USER%\Downloads\WinEscritorio-main\WinEscritorio-main || cd C:\Users\%USER%\Downloads\WinEscritorio-main
 echo         1.-Escritorio
 echo         2.-Cursor
 echo         3.-Salir
-set /p opcion=opcion a realizar: 
+set /p opcion=opcion a realizar:  
 if "%opcion%"=="1" goto operaciones
 if "%opcion%"=="2" goto grupos/usuarios
 if "%opcion%"=="3" goto salir
@@ -21,10 +23,11 @@ if "%opcion%"=="3" goto salir
 :operaciones
 color 03
 cls
+cd C:\Users\%USER%\Downloads\WinEscritorio-main\WinEscritorio-main\Escritorio || cd C:\Users\%USER%\Downloads\WinEscritorio-main\Escritorio
 echo      1.-Rainmiter
 echo      2.-Tema
 echo      3.-salir
-set /p opcion=opcion a realizar:
+set /p opcion=opcion a realizar: 
 if "%opcion%"=="1" goto sumar
 if "%opcion%"=="2" goto restar
 if "%opcion%"=="3" goto inicio
@@ -34,9 +37,12 @@ Rainmeter-4.5.20.exe
 goto operaciones
 :restar
 color 06
-for /F "tokens=2 delims=\" %%i in ('whoami') do set USER=%%i
-xcopy "Escritorio\Rainmeter" "C:\Users\%USER%\Documents\" /E /COPYALL /R:0 /W:0
-xcopy "Escritorio\Roaming\Rainmeter" "C:\Users\%USER%\AppData\Roaming" /E /COPYALL /R:0 /W:0
+robocopy "Rainmeter" "C:\Users\%USER%\Documents\Rainmeter" /E 
+pause
+cd Roaming
+robocopy "Rainmeter" "C:\Users\%USER%\AppData\Roaming\Rainmeter" /E 
+
+pause
 goto operaciones
 
 
@@ -46,6 +52,7 @@ goto operaciones
 :grupos/usuarios
 color 02
 cls
+cd C:\Users\%USER%\Downloads\WinEscritorio-main\WinEscritorio-main\Cursor|| cd C:\Users\%USER%\Downloads\WinEscritorio-main\Cursor
 echo      1.-Blanco
 echo      2.-Negro
 echo      3.-Rojo
@@ -54,18 +61,23 @@ set /p opcion=opcion a realizar:
 if "%opcion%"=="1" goto usuario
 if "%opcion%"=="2" goto grupo
 if "%opcion%"=="3" goto otros
-if "%opcion%"=="4" goto inicio
+if "%opcion%"=="4" goto inicio1
 
 :usuario
 color 06
-rundll32 setupapi,InstallHinfSection DefaultInstall 132 "Cursor\light\purple\Install.inf"
+cd light\purple
+rundll32 setupapi,InstallHinfSection DefaultInstall 132 "./Install.inf"
 goto grupos/usuarios
 :grupo
-rundll32 setupapi,InstallHinfSection DefaultInstall 132 "Cursor\dark\purple\Install.inf"
+cd dark\purple
+rundll32 setupapi,InstallHinfSection DefaultInstall 132 "./Install.inf"
 goto grupo/usuarios
 :otros
-rundll32 setupapi,InstallHinfSection DefaultInstall 132 "Cursor\red\Install.inf"
-rundll32 setupapi,InstallHinfSection DefaultInstall 132 "Cursor\dark\purple\Install.inf"
+cd red
+rundll32 setupapi,InstallHinfSection DefaultInstall 132 "./Install.inf"
+cd ..
+cd dark\purple
+rundll32 setupapi,InstallHinfSection DefaultInstall 132 "./Install.inf"
 goto grupos/usuarios
 
  
